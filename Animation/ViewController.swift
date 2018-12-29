d40a89d1ac18203c9c124d8861538cec4587a35d
舟木正憲 <funakimfunaki@gmail.com>
2018-12-29 12:39:31 +0900

    アニメーションの実装

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func buttonTapped(sender: UIButton) {
+        // animate は UIView が持つクラスメソッド
+//        UIView.animate(withDuration: 1.0) {
+//            self.imageView.transform = CGAffineTransform(translationX: 0, y: -100)
+//        }
+
+//        UIView.animate(withDuration: 1.0, animations: {
+//            self.imageView.transform = CGAffineTransform(translationX: 0, y: -100)
+//            // 引数をち使わない時は _ を使う
+//        }) { (_) in
+//            // identity は元の位置に戻る
+//            self.imageView.transform = CGAffineTransform.identity
+//        }
+
+//        UIView.animate(withDuration: 1.0, animations: {
+//            self.imageView.transform = CGAffineTransform(translationX: 0, y: -100)
+//            // 引数をち使わない時は _ を使う
+//        }) { (_) in
+//            UIView.animate(withDuration: 1.0
+//                , animations: {
+//                    self.imageView.transform = CGAffineTransform.identity
+//            })
+//        }
+
+        UIView.animate(withDuration: 1.0, delay: 0, usingSpringWithDamping: 0.1, initialSpringVelocity: 0.1, options: .curveLinear, animations: {
+            self.imageView.transform = CGAffineTransform(translationX: 0, y: -100)
+        }) { (_) in
+            UIView.animate(withDuration: 1.0
+              , animations: {
+               self.imageView.transform = CGAffineTransform.identity
+            })
+        }
+
+    }
 
 }
 
